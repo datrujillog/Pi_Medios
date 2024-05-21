2a0fe15ad7e864bfda0c0ebb79b87a82027e439d
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-21 18:13:21 -0500

    feat: Add user creation route to role router

--- api/src/router/roleRouter.js
+++ api/src/router/roleRouter.js
@@ -1,28 +1,52 @@
 import express from "express";
+// import userService from '../service/userService.js'; 
 
 
-
-class UserRouter {
+class RoleRouter {
     static #instance;
 
     constructor() {
-        if (!UserRouter.#instance) {
-            UserRouter.#instance = this;
+        if (!RoleRouter.#instance) {
+            RoleRouter.#instance = this;
             this.router = express.Router();
             this.setupRoutes();
         }
 
-        return UserRouter.#instance;
+        return RoleRouter.#instance;
     }
 
     setupRoutes() {
+        this.router.post("/create", async (req, res) => {
 
+            try {
+
+                const body = req.body;
+                const response = await userService.createUser(body);
+                return res.status(201).json({
+                    success: true,
+                    message: "User created successfully",
+                    data: response
+                });
+
+            } catch (error) {
+                console.log(error);
+            }
+
+        });
+
+
+
+
+
+
+
+        // Puedes agregar más rutas aquí si es necesario
     }
-    
+
     getRouter() {
         return this.router;
     }
 }
 
-const UserRouterInstance = new UserRouter();
-export default UserRouterInstance.getRouter();
+const RoleRouterInstance = new RoleRouter();
+export default RoleRouterInstance.getRouter();
