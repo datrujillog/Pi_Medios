a75d02de5c3a9ef5f75a727c43b91274c61698df
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-21 14:35:04 -0500

    feat: Load products data into the database

---
+++ api/src/database/loadData.js
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+import  products  from './data/products.js';
+// import products from '../data/products.js';
+
+const prisma = new PrismaClient();
+
+const loadProducts = async () => {
+    for (const product of products) {
+        await prisma.product.create({ data: product });
+    }
+    console.log('Products loaded');
+};
+
+loadProducts().catch(e => {
+    console.error(e);
+    process.exit(1);
+}).finally(async () => {
+    await prisma.$disconnect();
+});
+
