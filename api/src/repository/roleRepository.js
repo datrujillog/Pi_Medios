c4df4503b85416907fb18f904416e4960010193c
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-21 19:15:45 -0500

    chore: Add error handling for role creation in RoleRepository

--- api/src/repository/roleRepository.js
+++ api/src/repository/roleRepository.js
@@ -1,14 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { BadRequest } from "../middleware/error.js";
 
 
 class RoleRepository {
-    static #instance; 
+    static #instance;
 
     #roleModel;
 
     constructor() {
         if (!RoleRepository.#instance) {
-           
+
             RoleRepository.#instance = this;
             this.#roleModel = new PrismaClient().role;
         }
@@ -18,11 +19,42 @@ class RoleRepository {
 
     async createRole(body) {
 
-        const user = await this.#roleModel.create({
-           data: body,
-        });
+        try {
 
-        return user;
+            const user = await this.#roleModel.create({
+                data: body,
+            });
+
+            return user;
+        } catch (error) {
+            throw new BadRequest(error);
+        }
+
+
+    }
+
+    async assignRole(body) {
+
+        try {
+
+            const role = await this.#roleModel.update({
+                where: {
+                    id: body.roleId
+                },
+                data: {
+                    users: {
+                        connect: {
+                            id: body.userId
+                        }
+                    }
+                }
+            });
+
+            return role;
+        } catch (error) {
+            throw new BadRequest(error);
+        }
+
     }
 }
 
