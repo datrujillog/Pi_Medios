373b8584b715894fec541b0a01a9e6e656c7a478
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-21 23:45:46 -0500

    feat: Refactor date handling in saleService
    This commit refactors the date handling in the saleService module to ensure consistent and accurate calculation of sales. The code changes update the logic for setting the start and end dates, improving the precision of the calculations.

--- api/src/service/saleService.js
+++ api/src/service/saleService.js
@@ -41,12 +41,20 @@ class SaleService {
 
 
         const startDate = new Date(date);
-        startDate.setUTCHours(0, 0, 0, 0);  
+        startDate.setUTCHours(0, 0, 0, 0);
         const endDate = new Date(startDate);
-        endDate.setUTCHours(23, 59, 59, 999); 
+        endDate.setUTCHours(23, 59, 59, 999);
 
 
         const sales = await saleRepository.totalSales(startDate, endDate);
+        return sales;
+    }
+    
+    async totalSalesMonth(year, month) {
+        const startDate = new Date(Date.UTC(year, month - 1, 1));
+        const endDate = new Date(Date.UTC(year, month, 1));
+
+        const sales = await saleRepository.totalSalesMonth(startDate, endDate);
         return sales;
     }
 }
