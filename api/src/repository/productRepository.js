88b42155f46900621ef64f1084cfa14a1348c3d7
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-21 20:00:39 -0500

    feat: Implement createProduct method in ProductRepository

---
+++ api/src/repository/productRepository.js
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+import { BadRequest } from "../middleware/error.js";
+
+
+class ProductRepository {
+    static #instance;
+
+    #productModel;
+
+    constructor() {
+        if (!ProductRepository.#instance) {
+
+            ProductRepository.#instance = this;
+            this.#productModel = new PrismaClient().product;
+        }
+
+        return ProductRepository.#instance;
+    }
+
+    async createProduct(body) {
+
+        try {
+
+            const productResults = await this.#productModel.create({
+                data: body,
+            });
+            return productResults;
+
+        } catch (error) {
+            throw new BadRequest(error);
+        }
+    }
+
+}
+
+export default new ProductRepository;
