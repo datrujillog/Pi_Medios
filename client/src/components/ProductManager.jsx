7ffb04ee3f6a8220136a6e4e192774441ca743de
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-22 13:24:38 -0500

    feat: Add modal for creating new user in ProductManager component

--- client/src/components/ProductManager.jsx
+++ client/src/components/ProductManager.jsx
@@ -4,19 +4,25 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
 import Table from 'react-bootstrap/Table';
+import Modal from 'react-bootstrap/Modal';
+import { Button, Form } from 'react-bootstrap';
+import { sendContact } from '../api/api';
+
 
 
 
 
 export const ProductManager = () => {
     const [users, setUsers] = useState([]);
+    const [showCreateModal, setShowCreateModal] = useState(false);
+    const [newUser, setNewUser] = useState(null);
 
-    
+
     const getProduct = async () => {
         try {
             const response = await axios.get('http://localhost:5000/api/v1/products/list', {
                 headers: {
-                    'Auth': 'abcda0dc-74fd-4e3c-8316-a3df6f5071a6',
+                    'Auth': '7e3224dd-6530-485a-a4a4-9d342bc30c76',
                 }
             });
             setUsers(response.data.data);
@@ -29,11 +35,42 @@ export const ProductManager = () => {
         getProduct();
     }, []);
 
+
+
+    //modal create user
+
+    const hadleCreate = () => {
+        setNewUser({ id: '', name: '', description: '', price: '' });
+        setShowCreateModal(true);
+    };
+
+
+    const handleCreateInputChange = (event) => {
+        if (newUser) {
+            setNewUser({
+                ...newUser,
+                [event.target.name]: event.target.value,
+            });
+        }
+    };
+
+    const handleCreateUser = async () => {
+        if (newUser) {
+            // await axios.post(`http://localhost:5000/api/users`, newUser);
+            sendContact(newUser);
+            setNewUser(null);
+            getProduct();
+        }
+        setShowCreateModal(false);
+    };
+
+
+
     return (
         <>
 
             <Container className='mt-4'>
-            <div>ProductManager</div>
+                <div>ProductManager</div>
                 <Row>
                     <Col lg={8} xs={12} className='mx-auto'>
                         <Table striped bordered hover variant="" >
@@ -53,19 +90,54 @@ export const ProductManager = () => {
                                         <td>{index + 1}</td>
                                         <td>{product.name}</td>
                                         <td>{product.description}</td>
-                                        <td>{product.price}</td>
+                                        <td>$ {product.price}</td>
                                     </tr>
                                 ))}
                             </tbody>
 
                         </Table>
                     </Col>
-                    <button className='btn btn-success ' style={{ marginRight: '10px' }}>Create</button>
-                    <button className='btn btn-primary my-4 ' style={{ marginRight: '10px' }}>Reload</button>
+                    <button className='btn btn-success ' style={{ marginRight: '10px' }} onClick={() => hadleCreate()}>Create</button>
+
+                    <button className='btn btn-primary my-4 ' style={{ marginRight: '10px' }} onClick={getProduct}>Reload</button>
                 </Row>
 
 
             </Container>
+
+            {/* Modal para crear usuario */}
+
+            <Modal show={showCreateModal} onHide={() => setShowCreateModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Create User</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formUsername">
+                            <Form.Label>name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter name" name="name" onChange={handleCreateInputChange} />
+                        </Form.Group>
+
+                        <Form.Group controlId="formEmail">
+                            <Form.Label>description</Form.Label>
+                            <Form.Control type="text" placeholder="Enter description" name="description" onChange={handleCreateInputChange} />
+                        </Form.Group>
+
+                        <Form.Group controlId="formEmail">
+                            <Form.Label>price</Form.Label>
+                            <Form.Control type="text" placeholder="Enter price" name="price" onChange={handleCreateInputChange} />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowCreateModal(false)}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleCreateUser}>
+                        Create Usercghjf
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
 
 
