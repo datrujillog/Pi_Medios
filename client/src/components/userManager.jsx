5258a2b1f0764b81e5714cff90ed0c79e7e133e5
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-22 16:30:53 -0500

    feat: Add assign role functionality to UserManager component
    This commit adds the functionality to assign roles to users in the UserManager component. It includes the addition of a new modal for assigning roles, as well as the necessary state variables and event handlers. This change aligns with recent user commits and follows the established commit message conventions in the repository.

--- client/src/components/userManager.jsx
+++ client/src/components/userManager.jsx
@@ -17,6 +17,11 @@ export const UserManager = () => {
   const [showCreateModal, setShowCreateModal] = useState(false);
   const [newUser, setNewUser] = useState(null);
 
+  const [showAssigRoleModal, setShowAssigRoleModal] = useState(false);
+  const [newRole, setNewRole] = useState(null);
+
+
+
   const userAdmin = 'abcda0dc-74fd-4e3c-8316-a3df6f5071a6';
   const userEmployee = '7e3224dd-6530-485a-a4a4-9d342bc30c76';
 
@@ -43,11 +48,63 @@ export const UserManager = () => {
     }
   }
 
-  
+
 
   useEffect(() => {
     getUsers();
   }, []);
+
+  //modal assign role
+
+  const hadleAssigRole = () => {
+    setNewRole({ id: '', userId: '', roleId: '' });
+    setShowAssigRoleModal(true);
+  };
+
+
+  const handleAssigRoleInputChange = (event) => {
+    if (newRole) {
+      setNewUser({
+        ...newRole,
+        [event.target.name]: event.target.value,
+      });
+    }
+  };
+
+
+  const handleAssigRole = async () => {
+    if (newUser) {
+      console.log('newRole', newRole);
+
+      await axios.put('http://localhost:5000/api/v1/roles/assignUser',
+        newUser,
+        {
+          headers: {
+            // 'Auth': userEmployee, //employee
+            'Auth': userAdmin
+          }
+        }
+      ).then((response) => {
+        console.log('response', response);
+        getUsers();
+        setNewRole(null);
+      }).catch((error) => {
+        console.log('error', error);
+        const MySwal = withReactContent(Swal)
+        MySwal.fire({
+          title: 'Error',
+          text: error.response.data.message || error.response.data,
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        })
+      });
+      setShowCreateModal(false);
+    }
+  };
+
+
+
+
 
 
 
@@ -57,7 +114,6 @@ export const UserManager = () => {
     setNewUser({ id: '', name: '', lastName: '', document: '', rolesId: '' });
     setShowCreateModal(true);
   };
-
 
   const handleCreateInputChange = (event) => {
     if (newUser) {
@@ -97,7 +153,7 @@ export const UserManager = () => {
     }
   };
 
-console.log('users', users);
+  console.log('users', users);
 
   return (
     <>
@@ -134,6 +190,8 @@ console.log('users', users);
             </Table>
           </Col>
           <button className='btn btn-success ' style={{ marginRight: '10px' }} onClick={() => hadleCreate()}>Create</button>
+
+          <button className='btn btn-success mt-4 ' style={{ marginRight: '10px' }} onClick={() => hadleAssigRole()}>Assignar Role</button>
 
           <button className='btn btn-primary my-4 ' style={{ marginRight: '10px' }} onClick={getUsers}>Reload</button>
         </Row>
@@ -176,6 +234,36 @@ console.log('users', users);
           </Button>
           <Button variant="primary" onClick={handleCreateUser}>
             Create Users
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+
+      {/* Modal para asignar roles */}
+
+      <Modal show={showAssigRoleModal} onHide={() => setShowAssigRoleModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label>userId</Form.Label>
+              <Form.Control type="text" placeholder="Enter userId" name="userId" onChange={handleAssigRoleInputChange} />
+            </Form.Group>
+
+            <Form.Group controlId="formUserlastNanme">
+              <Form.Label>roleId</Form.Label>
+              <Form.Control type="text" placeholder="Enter roleId" name="roleId" onChange={handleAssigRoleInputChange} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowCreateModal(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleAssigRole}>
+            Asignar Role
           </Button>
         </Modal.Footer>
       </Modal>
