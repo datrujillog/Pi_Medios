043cf5050425295784b7994bb37c17e51a26e9f7
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-22 14:31:31 -0500

    feat: Add userAdmin and userEmployee constants in ProductManager component
    This commit adds the userAdmin and userEmployee constants in the ProductManager component. These constants are used to define the user roles for authentication purposes. This change aligns with recent user commits and follows the established commit message conventions in the repository.

--- client/src/components/ProductManager.jsx
+++ client/src/components/ProductManager.jsx
@@ -17,6 +17,9 @@ export const ProductManager = () => {
     const [showCreateModal, setShowCreateModal] = useState(false);
     const [newUser, setNewUser] = useState(null);
 
+    const userAdmin = 'abcda0dc-74fd-4e3c-8316-a3df6f5071a6';
+    const userEmployee = 'e3224dd-6530-485a-a4a4-9d342bc30c76';
+
 
     const getProduct = async () => {
         try {
@@ -27,6 +30,14 @@ export const ProductManager = () => {
             });
             setUsers(response.data.data);
         } catch (error) {
+            const MySwal = withReactContent(Swal)
+            MySwal.fire({
+                title: 'Error',
+                text: error.response.data.message,
+                icon: 'error',
+                confirmButtonText: 'Ok'
+            })
+
             console.log(error);
         }
     }
@@ -61,8 +72,8 @@ export const ProductManager = () => {
                 newUser,
                 {
                     headers: {
-                        'Auth': '7e3224dd-6530-485a-a4a4-9d342bc30c76', //employee
-                        // 'Auth': 'abcda0dc-74fd-4e3c-8316-a3df6f5071a6', //admin
+                        // 'Auth': userEmployee, //employee
+                        'Auth': userAdmin
                     }
                 }
             ).then((response) => {
