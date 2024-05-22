00400256358055b65910682f439beb2657eca2d1
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-21 22:55:08 -0500

    feat: Refactor totalSales method in SaleRepository to improve accuracy

--- api/src/service/saleService.js
+++ api/src/service/saleService.js
@@ -3,7 +3,7 @@ import saleRepository from "../repository/saleRepository.js";
 
 
 class SaleService {
-    static #instance; 
+    static #instance;
 
     constructor() {
         if (!SaleService.#instance) {
@@ -14,7 +14,7 @@ class SaleService {
         return SaleService.#instance;
     }
 
-    async createSale(body) {        
+    async createSale(body) {
         const saleResults = await saleRepository.createSale(body);
         return saleResults;
     }
@@ -32,6 +32,22 @@ class SaleService {
     async deleteSale(id) {
         const sale = await saleRepository.deleteSale(id);
         return sale;
+    }
+
+    async totalSales(date) {
+        // const startDate = new Date(date);
+        // const endDate = new Date(date);
+        // endDate.setDate(endDate.getDate()+1);
+
+
+        const startDate = new Date(date);
+        startDate.setUTCHours(0, 0, 0, 0); 
+        const endDate = new Date(startDate);
+        endDate.setUTCHours(23, 59, 59, 999); 
+
+
+        const sales = await saleRepository.totalSales(startDate, endDate);
+        return sales;
     }
 }
 export default new SaleService();
