82a68d1eb77c0141fbfdfa05dd84c9840dbf796b
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-22 19:14:48 -0500

    refactor: Update UserAmin constant in api.js
    This commit updates the UserAmin constant in the api.js file of the client/src/components/api directory. The previous value has been replaced with a new value. This change aligns with recent user commits and follows the established commit message conventions in the repository.

--- client/src/components/api/api.js
+++ client/src/components/api/api.js
@@ -1,11 +1,9 @@
 
 
 //declar un avariable para la url de la api
-const UserAmin = 'abcda0dc-74fd-4e3c-8316-a3df6f5071a6';
+// const UserAmin = '1e03713c-852f-46f2-b279-f430eca224d3';
+const UserAmin = '7e3224dd-6530-485a-a4a4-9d342bc30c76';
 
-const UserEmployee = '7e3224dd-6530-485a-a4a4-9d342bc30c76';
+// const UserEmployee = '7e3224dd-6530-485a-a4a4-9d342bc30c76';
 
-export {
-    UserAmin,
-    UserEmployee
-};
+export default UserAmin;
